80f78de653dbecc111b78edf6a5e9b4ea03c1e9f
Pratyush Kumar Pandey <55307565+Pratyush0411@users.noreply.github.com>
2020-11-01 20:00:38 +0800

    Complaints insert script

---
+++ lab5/COMPLAINTS_INSERT.sql
@@ -0,0 +1,19 @@
+INSERT INTO COMPLAINTS(ID,Text,Status,Filed_date_time,Handled_date_time,EMPLOYEESID,USERID)
+
+VALUES
+
+(1,'Horrible Service.','addressed','10-04-2020 10:20 AM','10-06-2020 10:20 AM', 2, 37),
+(2,'Faulty Product.','addressed','10-05-2020 1:00 AM','10-08-2020 2:00 AM', 3, 61),
+(3, 'Slow delivery','addressed','10-06-2020 1:00 AM','10-07-2020 1:00 AM',1,43),
+(4, 'Bad shop','addressed','10-07-2020 1:00 AM','10-11-2020 1:00 AM',3,61),
+(5, 'Bad packet','addressed','10-09-2020 1:00 AM','10-11-2020 1:00 AM',2,37),
+(6, 'Rude delivery man','addressed','10-10-2020 1:00 AM','10-10-2020 3:00 AM',2,53),
+(7, 'Seal broken','addressed','10-10-2020 1:00 AM','10-11-2020 1:00 AM',1,37),
+(8, 'Bad product','addressed','10-11-2020 1:00 AM','10-14-2020 1:00 AM',1,43),
+(9, 'Broken product','addressed','10-15-2020 1:00 AM','10-19-2020 1:00 AM',4,61),
+(10, 'Charged extra','addressed','10-21-2020 1:00 AM','10-23-2020 1:00 AM',2,37),
+(11, 'Delivery man broke the pack','addressed','10-25-2020 1:00 AM','10-28-2020 1:00 AM',1,11),
+(12, 'never ordering from sharkee again','addressed','10-28-2020 1:00 AM','10-31-2020 9:00 AM',3,43),
+(13, 'Too late for delivery','addressed','10-31-2020 1:00 AM','11-4-2020 1:00 AM',4,43),
+(14, 'Delivery did not happen','pending','11-04-2020 1:00 AM',NULL,4,53),
+(15, 'Your company is a joke. Useless people in management. Bad experience','pending','11-05-2020 1:00 AM',NULL,4,45);
