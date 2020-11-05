9334980544f4d060544757b2e093bcfc504b7bc8
Zhang Yongzhe <jcddyongzhe@gmial.com>
2020-11-05 15:25:04 +0800

    for query 4& 7 demo

--- lab5/labQueries.sql
+++ lab5/labQueries.sql
@@ -6,6 +6,21 @@ GROUP BY C.EMPLOYEESID
 ORDER BY Latency) AS L
 WHERE E.ID = L.EMPLOYEESID;
 
+-- query 4, steps
+-- Create a temporary view for to find the employee ID with the shortest latency
+WITH shortest_Latency AS
+(
+SELECT TOP 1 EMPLOYEESID, AVG(datediff(second, Filed_date_time, Handled_date_time)) AS latency
+FROM COMPLAINTS
+GROUP BY EMPLOYEESID
+ORDER BY latency
+)
+
+-- Find the employee name
+SELECT name 
+FROM EMPLOYEES E, shortest_Latency S
+WHERE E.ID = S.EMPLOYEESID;
+
 
 
 --query 7
@@ -15,5 +30,21 @@ GROUP BY USERID
 ORDER BY numOfComplaints DESC) AS TopComplainer
 WHERE U.ID = TopComplainer.USERID AND U.ID = O.USERID AND PIO.ORDERSID = O.ID AND P.ID = PRODUCTSID
 ORDER BY Price_In_Order DESC;
+
+-- query 7, steps
+-- view of the user complaints most
+CREATE VIEW TopComplainer AS 
+SELECT TOP 1 USERID, COUNT(*) as numOfComplaints FROM COMPLAINTS
+GROUP BY USERID
+ORDER BY numOfComplaints DESC;
+
+-- view of the most expensive product purchased by top complainer
+CREATE VIEW mostExpensivePurchased AS
+SELECT TOP 1 PRODUCTSID FROM PRODUCTS_IN_ORDERS, TopComplainer WHERE ORDERSID = (SELECT ID FROM ORDERS WHERE USERID = TopComplainer.USERID)
+ORDER BY Price_In_Order DESC;
+
+-- Select the name of the top complainer and the most expensive purchased-product name 
+SELECT U.name AS userName, P.name AS productName FROM USERS AS U, PRODUCTS AS P, mostExpensivePurchased AS MEP, TopComplainer AS TP
+WHERE U.ID = TP.USERID AND P.ID = MEP.PRODUCTSID;
 
 
