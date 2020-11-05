7e0eca3c8f50a5e3524b830c25f04e2a8f5f9321
Pratyush Kumar Pandey <55307565+Pratyush0411@users.noreply.github.com>
2020-11-05 23:30:36 +0800

    Update Query5,6,9.sql

--- lab5/labQueries/Query5,6,9.sql
+++ lab5/labQueries/Query5,6,9.sql
@@ -1,8 +1,14 @@
 --query 5
+-- part 1 select where maker = 'Samsung'
+SELECT Name, ID 
+FROM PRODUCTS 
+WHERE maker = 'Samsung';
+-- part2
 WITH SAM_PRODUCTS AS 
 (SELECT Name, ID 
 FROM PRODUCTS 
 WHERE maker = 'Samsung')
+-- perform left join because we need even the products not sold by any shop
 SELECT SAM_PRODUCTS.Name, COUNT(PRODUCTS_IN_SHOPS.SHOPSID) 
 FROM SAM_PRODUCTS
 LEFT JOIN PRODUCTS_IN_SHOPS 
@@ -12,6 +18,7 @@ GROUP BY SAM_PRODUCTS.Name ;
 --query 6
 --finding revenue of all the shops in August 2020
 --used . operator for using distinguishing common attributes
+-- used IN operator to find orders in August 2020
 WITH A1 AS(
 SELECT PRODUCTS_IN_ORDERS.SHOPSID as S_ID,SUM(PRODUCTS_IN_ORDERS.Price_In_Order*PRODUCTS_IN_ORDERS.Quantity_In_Order) AS REVENUE
 FROM PRODUCTS_IN_ORDERS 
@@ -28,13 +35,14 @@ WHERE REVENUE = (SELECT MAX(REVENUE) FROM A1)
 AND S_ID = ID;
 
 --query 9
+-- finding purchases of every product for every month in every year
 create view T1 as(
 SELECT PRODUCTS_IN_ORDERS.PRODUCTSID, MONTH(ORDERS.Date_time) as mon, YEAR(ORDERS.Date_time)AS yr, SUM(PRODUCTS_IN_ORDERS.Quantity_In_Order) AS purchases
 FROM PRODUCTS_IN_ORDERS
 JOIN ORDERS ON PRODUCTS_IN_ORDERS.ORDERSID = ORDERS.ID
 GROUP BY PRODUCTS_IN_ORDERS.PRODUCTSID, MONTH(ORDERS.Date_time), YEAR(ORDERS.Date_time))
 go
-
+-- finding products increasing sold over the past 3 months
 create view T2 AS(
 SELECT  distinct A1.PRODUCTSID
 FROM T1 A1, T1 A2, T1 A3
@@ -45,7 +53,7 @@ OR (A1.yr = A2.yr AND A2.yr = A3.yr-1 AND A1.mon=11 AND A2.mon = 12 AND A3.mon =
 AND (A1.purchases > A2.purchases AND A2.purchases > A3.purchases)
 )
 go
-
+-- finding product name for those products
 SELECT Name 
 From T2, PRODUCTS
 WHERE T2.PRODUCTSID = PRODUCTS.ID;
