b277bb116de44ebbdd75a0bbb8b57598fe74a82f
tcj-3x848 <70797116+tcj-3x848@users.noreply.github.com>
2020-11-06 14:04:17 +0800

    Create SHOPS.sql

---
+++ lab5/Insert_Scripts/SHOPS.sql
@@ -0,0 +1,17 @@
+
+SET IDENTITY_INSERT SHOPS ON;
+
+INSERT INTO SHOPS([ID],[sName]) VALUES(1,'Skajo'),(2,'Cenditel'),(3,'Yadel'),(4,'Centizu'),(5,'Edgeify'),(6,'Skiba'),(7,'Skyble'),(8,'Browseblab'),(9,'Npath'),(10,'Linktype');
+INSERT INTO SHOPS([ID],[sName]) VALUES(11,'Aibox'),(12,'Voonyx'),(13,'Flashset'),(14,'Dabtype'),(15,'Jaxnation'),(16,'Agivu'),(17,'Skipfire'),(18,'Twitternation'),(19,'Jaxspan'),(20,'Kwilith');
+INSERT INTO SHOPS([ID],[sName]) VALUES(21,'Roomm'),(22,'Topiczoom'),(23,'Zava'),(24,'Gabtype'),(25,'Jaxworks'),(26,'Twimbo'),(27,'Skinte'),(28,'Skinte'),(29,'Twimm'),(30,'Flipbug');
+INSERT INTO SHOPS([ID],[sName]) VALUES(31,'Ntag'),(32,'Photobean'),(33,'Tavu'),(34,'Demizz'),(35,'Innotype'),(36,'Feedmix'),(37,'Bubbletube'),(38,'Rhyloo	'),(39,'Jaxworks'),(40,'Livetube');
+INSERT INTO SHOPS([ID],[sName]) VALUES(41,'Mita'),(42,'Abatz'),(43,'Voonte'),(44,'Gigashots'),(45,'Devshare'),(46,'Twitterbeat'),(47,'Gigaclub'),(48,'Fatz'),(49,'Vidoo'),(50,'Voomm');
+INSERT INTO SHOPS([ID],[sName]) VALUES(51,'Shuffletag'),(52,'Cogibox'),(53,'Twitterwire'),(54,'Talane'),(55,'Katz'),(56,'Wikizz'),(57,'Yakidoo'),(58,'Trupe'),(59,'Bluezoom'),(60,'Oyoloo');
+INSERT INTO SHOPS([ID],[sName]) VALUES(61,'Skinix'),(62,'Rhycero'),(63,'Twimbo'),(64,'Mynte'),(65,'Dabshots'),(66,'Feedfire'),(67,'Devify'),(68,'Topicware'),(69,'Wikivu'),(70,'Babblestorm');
+INSERT INTO SHOPS([ID],[sName]) VALUES(71,'Devpulse'),(72,'Chatterpoint'),(73,'Edgeify'),(74,'Muxo'),(75,'Gabvine'),(76,'Jabberbean'),(77,'Twiyo'),(78,'Blogtag'),(79,'Skalith'),(80,'Youbridge');
+INSERT INTO SHOPS([ID],[sName]) VALUES(81,'Yadel'),(82,'Thoughtstorm'),(83,'Rhyzio'),(84,'Gigabox'),(85,'Fivebridge'),(86,'Mydo'),(87,'Linkbridge'),(88,'Buzzster'),(89,'Gabtune'),(90,'Youspan');
+INSERT INTO SHOPS([ID],[sName]) VALUES(91,'Thoughtblab'),(92,'Meetz'),(93,'Photobug'),(94,'Yakidoo'),(95,'Teklist'),(96,'Mydeo'),(97,'Kare'),(98,'Trudeo'),(99,'Oyoyo'),(100,'Omba');
+INSERT INTO SHOPS([ID],[sName]) VALUES(101,'Youspan'),(102,'Skiba'),(103,'Voonder'),(104,'Photobug'),(105,'Skyvu'),(106,'Skiba'),(107,'Flashdog'),(108,'Roombo'),(109,'Bluejam'),(110,'Mymm');
+INSERT INTO SHOPS([ID],[sName]) VALUES(111,'Thoughtstorm'),(112,'Tagchat'),(113,'Voolia'),(114,'Browsecat'),(115,'Eadel'),(116,'Jaxspan'),(117,'Mynte'),(118,'Feedfish'),(119,'Pixonyx'),(120,'Brainsphere');
+
+SET IDENTITY_INSERT SHOPS OFF;
