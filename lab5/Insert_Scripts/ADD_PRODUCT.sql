5740739f1661f0a48d1b4f0b2a0025ee1b2a0f62
Pratyush Kumar Pandey <55307565+Pratyush0411@users.noreply.github.com>
2020-11-06 11:15:56 +0800

    Add files via upload

---
+++ lab5/Insert_Scripts/ADD_PRODUCT.sql
@@ -0,0 +1,12 @@
+SET  IDENTITY_INSERT PRODUCTS ON
+
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (121,'Galaxy Note A','Samsung','Phone');
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (122,'Galaxy S4 active','Samsung','Phone');
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (123,'Galaxy S2','Samsung','Phone');
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (124,'Galaxy S3 active','Samsung','Phone');
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (125,' Galaxy A8 Note','Samsung','Phone');
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (126,'Galaxy A10','Samsung','Phone');
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (127,'galaxy tab s2','Samsung','Phone');
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (128,'galaxy A9 pro','Samsung','Phone');
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (129,'galaxy A5 note','Samsung','Phone');
+INSERT INTO PRODUCTS (ID, Name, Maker, Category) VALUES (130,'Galaxy Note B','Samsung','Phone');
